a94c9da9c7dda359e4be994a2cf3a38cc401657e
Eunbee Kim <ekim105@myseneca.ca>
2020-09-30 15:28:41 -0400

    issue-2 fixed

--- src/UrlCheck.java
+++ src/UrlCheck.java
@@ -4,22 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-
-interface Kernel32 extends Library {
-	
-	boolean SetConsoleTextAttribute(int h_ConsoleOutput, int u16_Attributes);
-
-	int GetStdHandle(int u32_Device);
-
-}
 
 public class UrlCheck {
 		// url set regex
@@ -27,112 +16,7 @@ public class UrlCheck {
 
 		// delimiter to get url from input file 
 		final static String delimiter = "[\\[\\]\"<>'\n\b\r]";
-		
-		//text for mac
-//	    public static final String RED = "\033[0;31m";
-//	    public static final String GREEN = "\033[0;32m";
-//	    public static final String WHITE = "\033[0;37m";
-//	    public static final String BLUE ="\033[0;34m";
-//	    public static final String RESET = "\033[0m";
-
-		// text for window
-		static final int GRAY = 0x7;
-		static final int GREEN = 0xA;
-		static final int RED = 0xC;
-		static final int WHITE = 0xF;
-		static final int BLUE = 0x9;
-		static final int STD_OUTPUT_HANDLE = -11;
-
-	    
-		// request url and check the response
-		public static void availableURL(String host)
-		{
-			Kernel32 lib = Native.load("kernel32", Kernel32.class);
-
-				try {
-						// request url
-						URL url = new URL(host);
-						URLConnection con;
-						con = url.openConnection();
-						
-						// response
-						HttpURLConnection exitCode = (HttpURLConnection) con;
-						exitCode.setInstanceFollowRedirects(true);
-						HttpURLConnection.setFollowRedirects(true);
-						exitCode.setConnectTimeout(1000);
-						
-						
-						// response code result
-						if(exitCode.getResponseCode() >= 200 && exitCode.getResponseCode() < 300)
-						{ 
-							
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), GREEN);
-							System.out.println("["+exitCode.getResponseCode()+"] "+ host +" - Good");							
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
-						}
-						else if(exitCode.getResponseCode() >= 400 && exitCode.getResponseCode() < 500)
-						{
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), RED);
-							System.out.println("["+exitCode.getResponseCode()+"] "+ host +" - Bad");					
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
-						}
-						else if(exitCode.getResponseCode() == 301 || exitCode.getResponseCode() == 307 || exitCode.getResponseCode() == 308 )
-						{
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), BLUE);
-							System.out.println("["+exitCode.getResponseCode()+"] "+ host +" - Redirect");
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
-							
-							// redirect to new location by Recursion itself when it is 301,307,308
-							String newUrl = exitCode.getHeaderField("Location");
-							availableURL(newUrl);	
-							
-						}
-						else
-						{
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), GRAY);
-							System.out.println("["+exitCode.getResponseCode()+"] "+ host +" - Unknown");	
-							lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
-						}
-
-				}catch (Exception e) {
-					// response fail, server is not existed
-					lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), RED);
-					System.out.println("[599] "+ host +" - Fail" );
-					lib.SetConsoleTextAttribute(lib.GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
-				}
-//						// for Mac
-//						if(exitCode.getResponseCode() >= 200 && exitCode.getResponseCode() < 300)
-//						{ 
-//
-//							System.out.println(GREEN+"["+exitCode.getResponseCode()+"] "+ host +" - Good"+RESET);							
-//						
-//						}
-//						else if(exitCode.getResponseCode() >= 400 && exitCode.getResponseCode() < 500)
-//						{
-//							System.out.println(RED+"["+exitCode.getResponseCode()+"] "+ host +" - Bad"+RESET);					
-//						}
-//						else if(exitCode.getResponseCode() == 301 || exitCode.getResponseCode() == 307 || exitCode.getResponseCode() == 308 )
-//						{
-//							System.out.println(BLUE + "["+exitCode.getResponseCode()+"] "+ host +" - Redirect"+ RESET);
-//							
-//							// redirect to new location by Recursion itself when it is 301,307,308
-//							String newUrl = exitCode.getHeaderField("Location");
-//							availableURL(newUrl);	
-//							
-//						}
-//						else
-//						{
-//							System.out.println(RED+"["+exitCode.getResponseCode()+"] "+ host +" - Unknown"+RESET);	
-//						}
-//
-//				}catch (Exception e) {
-//					// response fail, server is not existed
-//					System.out.println(RED+"[599] "+ host +" - Fail" +RESET);
-//
-//				}		
-						
-
-		}
+			
 		
 		public static void helpMessage()
 		{
@@ -147,7 +31,7 @@ public class UrlCheck {
 			System.out.println("|                                                      |");
 			System.out.println("| 2) UrlCheck help                                     |");
 			System.out.println("|                                                      |");
-			System.out.println("| 3) Option a, s, v                                    |");
+			System.out.println("| 3) Option a, s, v, m                                 |");
 			System.out.println("|                                                      |");
 			System.out.println("|  For Window :                                        |");
 			System.out.println("|                                                      |");
@@ -168,6 +52,11 @@ public class UrlCheck {
 			System.out.println("|                                                      |");
 			System.out.println("|                                                      |");
 			System.out.println("|  For Mac :                                           |");
+			System.out.println("|                                                      |");
+			System.out.println("|  Option m : To run the tool in Mac                   |");
+			System.out.println("|                                                      |");
+			System.out.println("|  UrlCheck /m <fileName>                              |");
+			System.out.println("|  ex) UrlCheck /m index2.html                         |");
 			System.out.println("|                                                      |");
 			System.out.println("|  Option a : To check for archived versions of URLs   |");
 			System.out.println("|                                                      |");
@@ -203,13 +92,13 @@ public class UrlCheck {
 		
 
 		// list up url in input file
-		public static void fileUrlListUp(String fName, boolean archived, boolean secured)
+		public static void fileUrlListUp(String fName, boolean archived, boolean secured, boolean runMac)
 		{
 			String regSecure = "^(http)://";
 			Pattern pat = Pattern.compile(regex, Pattern.MULTILINE);
 			
 			BufferedReader br;
-			
+
 			try {
 				br = new BufferedReader(new FileReader(fName));
 				String line = null;
@@ -223,8 +112,15 @@ public class UrlCheck {
 						{
 							// change http to https
 							if(secured) str = str.replaceFirst(regSecure, "https://");	
-
-							availableURL(str);
+							
+							if(runMac)
+							{
+								UrlCheckForMac.availableURL(str);
+							}
+							else
+							{
+								UrlCheckForWindow.availableURL(str);
+							}
 							
 							// request archived
 							if(archived) archiveUrl(str);
@@ -277,6 +173,7 @@ public class UrlCheck {
 		
 		boolean archived = false;
 		boolean secured = false;
+		boolean runMac = false;
 		
 		if(args.length == 0 || args[0].toLowerCase().equals("help"))
 		{
@@ -301,12 +198,15 @@ public class UrlCheck {
 					// secure request flag handle
 					if(args[0].contains("s")) secured = true;
 					
-					if(archived || secured)
+					// secure request flag handle
+					if(args[0].contains("m")) runMac = true;
+					
+					if(archived || secured || runMac)
 					{
 						for(int i = 1; i < args.length; i++)
 						{
 							System.out.println("File :  " + args[i]);
-							fileUrlListUp(args[i],archived,secured);					
+							fileUrlListUp(args[i],archived,secured,runMac);					
 						}		
 					}
 				
@@ -316,7 +216,7 @@ public class UrlCheck {
 				for(int i = 0; i < args.length; i++)
 				{
 					System.out.println("File :  " + args[i]);
-					fileUrlListUp(args[i],archived,secured);					
+					fileUrlListUp(args[i],archived,secured,runMac);					
 				}
 			}
 		}

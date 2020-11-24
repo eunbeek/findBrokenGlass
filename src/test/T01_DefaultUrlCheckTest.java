5fc75aec5c0961fdd5d212e17d95e94e20811276
Eunbee <dmsql1678@gmail.com>
2020-11-24 17:50:43 -0500

    testing: updated file name with Test

--- src/test/T01_DefaultUrlCheckTest.java
+++ src/test/T01_DefaultUrlCheckTest.java
@@ -2,28 +2,25 @@ package test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
-import main.UrlCheck;
 import org.junit.jupiter.api.Test;
+import main.UrlCheck;
 
-class T003_HttpsRequestCheck {
+class T01_DefaultUrlCheckTest {
 
   @Test
-  @SuppressWarnings("static-access")
   void testMain() {
-    String[] args = {"-s", "index.html"};
+    String[] args = {"index.html"};
 
     UrlCheck urlCheck = new UrlCheck();
-
     try {
       urlCheck.main(args);
 
     } catch (IOException e) {
-      e.printStackTrace();
-      assertFalse(true);
+      assertFalse(false);
     }
 
     assertTrue(true);
   }
+
 }
